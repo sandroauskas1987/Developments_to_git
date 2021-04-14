698c2239ec88d9df6d46fd7f81fe32582a4aa166
Задорожний Александр <43403657+sandroauskas1987@users.noreply.github.com>
2021-04-14 13:59:25 +0300

    first commit

--- bot_vk/config.php
+++ bot_vk/config.php
@@ -1,6 +1,5 @@
 <?php
 define('BOT_LOGS_DIRECTORY','logs');
-
 define('CALLBACK_API_CONFIRMATION_TOKEN', '8d8ca0d7'); //Строка для подтверждения адреса сервера из настроек Callback API
 define('VK_API_ACCESS_TOKEN', '*************************************************************'); //Ключ доступа сообщества
 define('YANDEX_API_KEY', '*****************************************************'); //Ключ для доступа к Yandex Speech Kit
